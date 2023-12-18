52952ea3d055a77eeba8fa1edfdac1ac34bc8cdd
felilozi <felicialozinski97@gmail.com>
2023-12-18 10:24:17 -0300

    Segundo commit estoy clonando el repositorio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clone por primera ves mi proyecto
         </p>
         <a
           className="App-link"
