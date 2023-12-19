f6135977b4a0f02ecb5eb7331f3e76d378cc8a48
felilozi <felicialozinski97@gmail.com>
2023-12-19 15:29:51 -0300

    miToDoList Primera Parte Terminada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoList } from './components/TodoList';
+import { TodoAdd } from './components/TodoAdd';
+import { useTodo } from './hooks/useTodo';
 
 function App() {
+  const {
+    todos,
+    todosCount,
+    pedingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleCompleteTodo,
+    handleUpdateTodo
+  } = useTodo();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Clone por primera ves mi proyecto
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='card-to-do'>
+        <h1>Lista de Tareas</h1>
+        <div className='counter-todos'>
+          <h3>N° Tareas: <span>{todosCount}</span></h3>
+          <h3>Pendiente:<span>{pedingTodosCount}</span></h3>
+        </div>
+        <div className='add-todo'>
+          <h3>Agregar tarea</h3>
+          <TodoAdd handleNewTodo={handleNewTodo} />
+        </div>
+
+
+        <TodoList
+          todos={todos}
+          handleDeleteTodo={handleDeleteTodo}
+          handleCompleteTodo={handleCompleteTodo}
+          handleUpdateTodo={handleUpdateTodo}
+
+        />
+      </div>
+
+    </>
   );
 }
 
